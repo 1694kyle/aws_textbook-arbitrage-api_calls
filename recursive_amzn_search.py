d173a019a8fa48c9a81d387c3276243518e38186
1694kyle <kylebonnet@gmail.com>
2015-10-28 17:08:01 -0700

    init

--- recursive_amzn_search.py
+++ recursive_amzn_search.py
@@ -24,43 +24,18 @@ def write(text, fname):
         f.write('\n')
 
 
-def get_similar_items(asin, depth = 3):
-    global seen
-    # ending condition up front
-    # function bounces back with nothing if called below depth 0
-    write('{}{}'.format('\t' * depth, asin), log_file)
-    if depth < 0:
-        return []
-
-    # pull all items similar to item from api
-    try:
-        similar_items = api.similarity_lookup(asin)
-    except:
-        return []
-
-    # iterate over each new similar item
-    for new_item in similar_items.Items.Item:
-        write('{}{}'.format('\t' * depth, asin), log_file)
-        # add to your results list the return from recursive call
-        more = get_similar_items(new_item.ASIN.text, depth -1)
-
-        similar_items.extend(more)
-
-    return similar_items
-
-
 def findem(asin, seen=None, depth=3):
     depth -= 1
     if depth > 0:
-        if seen is None:
-            seen = set()
+        # if seen is None:
+        #     seen = set()
         try:
             response = api.similarity_lookup(asin, ResponseGroup='Large')
         except:
             response = api.item_lookup(asin, ResponseGroup='Large')
         found = [item for asin, item in {item.ASIN: item for item in response.Items.Item if item.ASIN.text not in seen}.iteritems()]
         trade_eligible_found = [item for item in found if trade_eligible(item) is not None]
-        seen.update([item.ASIN.text for item in trade_eligible_found])
+        # seen.update([item.ASIN.text for item in trade_eligible_found])
         for item in found:
             write('{}{}'.format('\t' * depth, asin), log_file)
             yield item
@@ -78,6 +53,10 @@ def trade_eligible(item):
 def check_profit(items):
     global profit_count
     for item in items:
+        if item.ASIN in seen:
+            continue
+        else:
+            seen.add(item.ASIN)
         if hasattr(item.ItemAttributes, 'TradeInValue'):
             try:
                 trade_value = item.ItemAttributes.TradeInValue.Amount / 100.0
@@ -131,29 +110,37 @@ def main(asin_key, max_depth):
     for row in asin_csv:
         asin = row[0]
         write('{}{}'.format('\t' * depth, asin), log_file)
-        next_asin_set = findem(asin)
+        next_asin_set = findem(asin, depth=max_depth)
         check_profit(next_asin_set)
-        # next_asin_set = get_similar_items(asin, max_depth)
-        # check_profit(next_asin_set)
 
 if __name__ == '__main__':
 
+    # boto connection
     conn = boto.connect_s3(os.environ['AWS_ACCESS_KEY'], os.environ['AWS_SECRET_KEY'])
     bucket = conn.get_bucket('textbook-arbitrage')
-    api_cols = ['trade_value', 'price', 'profit', 'roi', 'url']
+
+    # key parameters
     date = datetime.today().date().strftime('%m-%d-%Y')
     search_date = ''
+
+    # get key
     keys = bucket.list()
     latest_items_key = item_keys(keys)
 
+    # set up api
     api = API(locale='us')
+
+    # misc variables
     count = 0
     profit_count = 0
     seen = set()
     date = datetime.today().date()
+    items = []
+    depth = 3
+    api_cols = ['trade_value', 'price', 'profit', 'roi', 'url']
 
+    # set up output location
     LOCAL_OUTPUT_DIR = os.path.join(os.environ.get('HOME'), 'Desktop', 'Recursive Search Results')
-
     item_file = os.path.join(LOCAL_OUTPUT_DIR, 'Items', 'items - {}.csv'.format(date))
     log_file = os.path.join(LOCAL_OUTPUT_DIR, 'Logs', 'log - {}.csv'.format(date))
     profitable_file = os.path.join(LOCAL_OUTPUT_DIR, 'Profitable', 'profitable - {}.csv'.format(date))
@@ -166,53 +153,10 @@ if __name__ == '__main__':
     open(log_file, 'wb').close()
     open(profitable_file, 'wb').close()
 
-    items = []
-    depth = 3
-
+    # execution
     main(latest_items_key, depth)
 
+    # closeout
     if profit_count > 0:
         send_mail_via_smtp(profitable_file)
 
-
-# todo: connect this with latest dump from s3 and search similar products in addition to those items
-
-
-
-
-
-
-### Not searching enough items ###
-# def main():
-#     global count, items
-#     response = api.browse_node_lookup(465600)
-#     for child in response.BrowseNodes.BrowseNode.Children.BrowseNode:
-#
-#         write('{} ({})'.format(child.Name, child.BrowseNodeId), fname=log_file)
-#         r = api.item_search(search_index='Books', BrowseNode=child.BrowseNodeId, ResponseGroup='Large')
-#         page_count = 0
-#         for page in r.iterpages():
-#             page_count += 1
-#             write('*****PAGE {}*****'.format(page_count), fname=log_file)
-#             for item in page.Items.Item:
-#                 count += 1
-#                 write('\t{} - {}'.format(count, item.ASIN), fname=log_file)  # write to log file
-#
-#                 similar = similar_items(item.ASIN.text)
-#                 if similar is not None:
-#                     for similar_item in similar.Items.Item:
-#                         if similar_item.ASIN.text not in items and trade_eligible(similar_item) is not None:
-#                             count += 1
-#                             write('\t\t{} - {}'.format(count, similar_item.ASIN), fname=log_file)  # write to log file
-#                             write('{}'.format(similar_item.ASIN), fname=item_file)  # write to item file
-#                             items.append(similar_item.ASIN.text)
-#                         else:
-#                             continue
-#                 else:
-#                     continue
-#
-#                 if item.ASIN.text not in items and trade_eligible(item) is not None:
-#                     write('{}'.format(item.ASIN), fname=item_file)  # write to item file
-#                     items.append(item.ASIN.text)
-#
-#         print 'Finished {}'.format(child.Name)
